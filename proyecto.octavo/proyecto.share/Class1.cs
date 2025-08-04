67b97e1252c6b8835b859a3957dcfa0084042ab8
guido marcelo <guidomarcelochavez15@gmail.com>
2025-08-04 14:49:44 -0400

    arquitectura del proyecto

---
+++ proyecto.octavo/proyecto.share/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto.share
+{
+    public class Class1
+    {
+    }
+}
